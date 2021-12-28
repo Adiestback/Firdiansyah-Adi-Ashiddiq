d69029ef97c83a92bc1f95f28fa03e690b49005c
Adiestback <82642495+Adiestback@users.noreply.github.com>
2021-12-28 19:39:26 +0700

    Create error.php

---
+++ public/error.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div id="page">
+    <h1>Page Not Found</h1>
+    <p>We're very sorry, but the page you requested has not been found! It may have been moved or deleted.</p>
+    <p>I'm not blaming you, but have you checked your address bar? There might be a typo in the URL.</p>
+    <p>If there isn't, you could try searching my website for the content you were looking for:</p>
+    <?php get_search_form(); ?>
+    <p>Or maybe you were looking for one of my popular posts:</p>
+    <ul>
+        <li><a href="http://www.website.com/popular-post1">Popular Post 1</a></li>
+        <li><a href="http://www.website.com/popular-post2">Popular Post 2</a></li>
+        <li><a href="http://www.website.com/popular-post3">Popular Post 3</a></li>
+        <li><a href="http://www.website.com/popular-post4">Popular Post 4</a></li>
+    </ul>
+    <p>Alternatively, you can go to <a href="http://www.website.com">my home page</a> or <a href="http://www.website.com/archives">browse my archives</a></p>
+    <p>One last thing, if you're feeling so kind, please <a href="mailto:webmaster@website.com">tell me</a> about this error, so that I can fix it. Thanks!</p>
+</div>
+<?php get_sidebar(); get_footer(); ?>
